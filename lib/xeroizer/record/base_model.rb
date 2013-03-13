c59651e579a5414f2edc61b5c479d885bb28adc0
fimmtiu <dennis@funkplanet.com>
2013-03-13 10:33:39 -0700

    woop woop

--- lib/xeroizer/record/base_model.rb
+++ lib/xeroizer/record/base_model.rb
@@ -177,7 +177,7 @@ module Xeroizer
         def to_bulk_xml(records, builder = Builder::XmlMarkup.new(:indent => 2))
           tag = (self.class.optional_xml_root_name || model_name).pluralize
           builder.tag!(tag) do
-            records.map {|r| puts "YARR FOO #{r.to_xml}\nYARR BAR #{r.to_xml(builder)}\n"; r.to_xml(builder) }
+            records.map {|r| puts "YARR FOO #{r.object_id} #{r}\n#{r.to_xml}\nYARR BAR #{r.to_xml(builder)}\n"; r.to_xml(builder) }
           end.tap {|b| puts "FINAL BUILDER STUFF: #{b.inspect}\n" }
         end
 
