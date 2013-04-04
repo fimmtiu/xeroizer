d431569a9ea1acecd6a6ad78374e040d8962184d
Dennis Taylor <dennis.taylor@goclio.com>
2013-04-04 12:52:32 -0700

    zot

--- lib/xeroizer/record/base_model.rb
+++ lib/xeroizer/record/base_model.rb
@@ -180,9 +180,12 @@ module Xeroizer
         def parse_records(response, elements)
           elements.each do | element |
             new_record = model_class.build_from_node(element, self)
+            puts "ELEMENT = #{element.inspect}"
+            puts "VALUE = #{element.attribute('status').try(:value).inspect}"
             if element.attribute('status').try(:value) == 'ERROR'
               new_record.errors = []
               element.xpath('//ValidationError').each do |err|
+                puts "ERR = #{err.inspect}"
                 new_record.errors << err.text.gsub(/^\s+/, '').gsub(/\s+$/, '')
               end
             end
